f91ec59a9cb65ff5d9ce6e2b27b18a7704b1b9da
axocomm <axocomm@gmail.com>
2014-02-01 00:00:07 -0500

    Added PHP-FPM www.conf using vagrant user and group

--- puppet/modules/php5-fpm/manifests/init.pp
+++ puppet/modules/php5-fpm/manifests/init.pp
@@ -11,5 +11,14 @@ class php5-fpm {
     require    => Package['php5-fpm']
   }
 
+  file {'/etc/php5/fpm/pool.d/www.conf':
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    source  => 'puppet:///php5-fpm/www.conf',
+    notify  => Service['php5-fpm'],
+    require => Package['php5-fpm']
+  }
+
   include mysql::php5-mysql
 }
