94ecbc175d52bce6942b3d83cf0f1098d9ce581d
Zach Russell <Zach@protechig.com>
2014-09-10 09:29:45 -0400

    add header upload functionality

--- wp-content/themes/wptemple/functions.php
+++ wp-content/themes/wptemple/functions.php
@@ -3,9 +3,9 @@
 include_once( get_template_directory() . '/lib/init.php' );
 
 //* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
-define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
-define( 'CHILD_THEME_VERSION', '2.1.2' );
+define( 'CHILD_THEME_NAME', 'WP Temple Theme' );
+define( 'CHILD_THEME_URL', 'https://www.wptemple.com/');
+define( 'CHILD_THEME_VERSION', '1.0.0' );
 
 //* Enqueue Google Fonts
 add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
@@ -26,3 +26,12 @@ add_theme_support( 'custom-background' );
 
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
+
+//* Add support for header image upload
+add_theme_support( 'custom-header', array(
+    'default-text-color'        => 'ffffff',
+    'header-selector'           => '.header-image .site-header .wrap',
+    'no_header_text'            => TRUE,
+    'height'                    => 100,
+    'width'                     => 300,
+) );
