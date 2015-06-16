4b93465357b110706b4bea9957bf3454da9ed4f2
zovt <zovtly@gmail.com>
2015-06-16 12:50:41 -0400

    Fixed post box sizes
    * wp-content/themes/wptemple/front-page.php: The lower posts are now
      contained in row containers, read-more moved out of the grid-post-content-box
    
    * wp-content/themes/wptemple/sass/_layout.scss: Added rules for post
      rows, updated read-more

--- wp-content/themes/wptemple/front-page.php
+++ wp-content/themes/wptemple/front-page.php
@@ -14,46 +14,50 @@ add_action('genesis_loop', 'wpt_home_loop');
 
 
 function wpt_home_loop() {
-    $query = get_home_posts();
-    $front_page = get_query_var('paged') === 0;
-    $posts = ($query->have_posts()) ? $query->get_posts() : array();
+   $query = get_home_posts();
+   $front_page = get_query_var('paged') === 0;
+   $posts = ($query->have_posts()) ? $query->get_posts() : array();
 
-    if (!count($posts)) {
-        echo '<span class="well">No Posts</span>';
-        return;
-    }
-
-    $item_no = ($front_page) ? 0 : 1;
-    for ($i = 0; $i < count($posts); $i++) {
-        $post = $posts[$i];
-        if ($item_no++ === 0) { 
-            $extra_class = 'top';
-          $thumbnail_size = 'homepage-featured-large';
-        } else {
-          $thumbnail_size = 'homepage-featured';
-          if ($item_no & 1) {
+   if (!count($posts)) {
+      echo '<span class="well">No Posts</span>';
+      return;
+   }
+   
+   $item_no = ($front_page) ? 0 : 1;
+   for ($i = 0; $i < count($posts); $i++) {
+      $post = $posts[$i];
+      if ($item_no++ === 0) { 
+         $extra_class = 'top';
+         $thumbnail_size = 'homepage-featured-large';
+      } else {
+         $thumbnail_size = 'homepage-featured';
+         if ($item_no & 1) {
             $extra_class = 'even';
-          } else {
+         } else {
             $extra_class = 'odd';
-          }
-        }
+         }
+      }
 
-        $categories = get_the_category($post->ID);
-        $category_links = array_map(function ($cat) {
-            return '<a href="' . get_category_link($cat->term_id) . '">' . $cat->cat_name . '</a>';
-        }, $categories);
+      $categories = get_the_category($post->ID);
+      $category_links = array_map(function ($cat) {
+         return '<a href="' . get_category_link($cat->term_id) . '">' . $cat->cat_name . '</a>';
+      }, $categories);
 
-        $tags = get_the_tags($post->ID);
-        if (is_array($tags) && count($tags)) {
-            $tag_links = array_map(function ($tag) {
-                return '<a href="' . get_tag_link($tag->term_id) . '">' . $tag->name . '</a>';
-            }, $tags);
-        } else {
-            $tag_links = array('none');
-        }
+      $tags = get_the_tags($post->ID);
+      if (is_array($tags) && count($tags)) {
+         $tag_links = array_map(function ($tag) {
+            return '<a href="' . get_tag_link($tag->term_id) . '">' . $tag->name . '</a>';
+         }, $tags);
+      } else {
+         $tag_links = array('none');
+      }
 
       $comments = get_comments_number($post->ID);
       global $Genesis_Simple_Share;
+
+      if ( $extra_class === 'odd' ) {
+      	 echo '<div class="grid-post-row">';
+      }
 ?>
 <div class="grid-post-area <?php echo $extra_class;?>">
   <div class="grid-post-upper">
@@ -71,45 +75,48 @@ function wpt_home_loop() {
       <span class="grid-post-share-icons"><?php echo genesis_share_get_icon_output( 'home-page-post-' . $post->ID, $Genesis_Simple_Share->icons ) ?></span>
     </div>
     <p class="grid-post-excerpt"><?php echo (!empty($post->post_excerpt)) ? $post->post_excerpt : wp_trim_words($post->post_content); ?></p>
-    <div class="grid-post-read-more">
-      <a href="<?php echo get_permalink($post->ID); ?>">Read More</a>
-    </div>
+  </div>
+  <div class="grid-post-read-more">
+    <a href="<?php echo get_permalink($post->ID); ?>">Read More</a>
   </div>
 </div>
 <?php
-    }
-echo '<div class="pagination">';
-	genesis_posts_nav();
-	wp_reset_query();
+	if ( $extra_class === 'even' ) {
+	   echo '</div>';
 }
-
-echo '</div>';
+}
+echo '<div class="pagination">';
+  genesis_posts_nav();
+  wp_reset_query();
+  }
+  
+  echo '</div>';
 
 function get_grid_thumbnail($post, $use_placeholder, $size ) {
-  if (has_post_thumbnail($post->ID)) {
-    $thumbnail = get_the_post_thumbnail($post->ID, $size, array('class' => 'grid-post-thumbnail'));
-    return $thumbnail;
-  } else {
-    if ($use_placeholder) {
-      return '<img class="grid-post-thumbnail" src="' . get_stylesheet_directory_uri() . '/images/home-placeholder-' . $size . '.gif">';
-    } else {
-      return '';
-    }
-  }
+if (has_post_thumbnail($post->ID)) {
+$thumbnail = get_the_post_thumbnail($post->ID, $size, array('class' => 'grid-post-thumbnail'));
+return $thumbnail;
+} else {
+if ($use_placeholder) {
+return '<img class="grid-post-thumbnail" src="' . get_stylesheet_directory_uri() . '/images/home-placeholder-' . $size . '.gif">';
+} else {
+return '';
+}
+}
 }
 
 function get_home_posts() {
-    $page = get_query_var('paged');
-    $args = array(
-        'post_type'         => 'post',
-        'post_status'       => 'publish',
-        'posts_per_page'    => $page === 0 ? 5 : 6,
-        'orderby'           => 'date',
-        'order'             => 'DESC',
-        'paged'             => $page
-    );
+$page = get_query_var('paged');
+$args = array(
+'post_type'         => 'post',
+'post_status'       => 'publish',
+'posts_per_page'    => $page === 0 ? 5 : 6,
+'orderby'           => 'date',
+'order'             => 'DESC',
+'paged'             => $page
+);
 
-    return new WP_Query($args);
+return new WP_Query($args);
 }
 
 genesis();
